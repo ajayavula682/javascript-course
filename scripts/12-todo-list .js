c8ca77c157b9d00611e8cdaf78831c87f72a4bf1
avulaajaykumarreddy <ajayavula682@gmail.com>
2025-01-31 01:00:43 -0500

    added arrow functions for the following files

--- scripts/12-todo-list .js
+++ scripts/12-todo-list .js
@@ -10,26 +10,39 @@ renderTodoList();
 function renderTodoList()
 {
     let todoListHTML ='';
-    for(let i=0;i<todoList.length;i++){
-      const todoObject =todoList[i];
-      //destructuring const name=todoObject.name;
-      //destructuring const dueDate=todoObject.dueDate;
+
+    todoList.forEach((todoObject, index) =>{
+      
       const { name, dueDate }= todoObject;
       const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
       <button onclick="
-        todoList.splice(${i},1);
+        todoList.splice(${index},1);
         renderTodoList();
-        " class="delete-todo-button">Delete</button>
+        " class="delete-todo-button js-delete-todo-button">Delete</button>
         `;
       todoListHTML +=html;
-    }
+    });
     //console.log(todoListHTML);
     document.querySelector('.js-todo-list')
     .innerHTML =todoListHTML;
 
+    document.querySelectorAll('.js-delete-todo-button')
+      .forEach((deleteButton,index) => {
+        deleteButton.addEventListener('click',() => {
+          todoList.splice(${index},1);
+        renderTodoList();
+
+      });
+    //document.querrySelectorAll()--> that gives all the button in the DOM
+
 }
+
+document.querySelector('.js-add-todo-button')
+.addEventListener('click', ()=>{
+  addToDo();
+});
 
 function addToDo()
 {
